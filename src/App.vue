0d1fb7a040804a451d9883b7174c7468ca99b39e
Veeramanohar <vind.veeru@gmail.com>
2020-08-31 22:11:27 +0530

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,313 @@
+
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main id="app" @mousemove="mousemove">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <div class='head'>
+      <img src="./assets/logo.svg" id='logo'>
+      <h1 id='title'>Baja Music</h1>
+    </div><hr>
+
+    <section class="player">
+        <h2 class="song-title">{{ current.title }} - <span>{{ current.artist }}</span></h2>
+        <div class="controls">
+          <button class="prev" @click="prev"><i class="fa fa-backward"></i></button>
+          <button class="play" v-if="!isPlaying" @click="play"><i class="fa fa-play"></i></button>
+          <button class="pause" v-else @click="pause"><i class="fa fa-pause"></i></button>
+          <button class="next" @click="next"><i class="fa fa-forward"></i></button>
+        </div>
+    </section>
+    
+    <section class="products" @click="play">
+      <Product
+        v-for="product in products"
+        :key="product.color"
+        :product="product"
+      />
+    </section>
+  </main>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
+<script>
+
+import Product from './components/Product';
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    Product
+  },
+  data () {
+    return {
+      current: {},
+      index: 0,
+      isPlaying: false,
+      products: [
+        {
+          title: 'Song 1',
+          color: 'pink',
+          bgtext: 'NIKE',
+          imgsrc: require('./assets/blue-shoe.png'),
+          artist:'Someone good singer',
+          src:'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-8.mp3',
+          time:'5:00'
+        },
+        {
+          title: 'Song 2',
+          color: 'pink',
+          bgtext: 'AIR',
+          artist:'Someone good singer',
+          src:'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-8.mp3',
+          imgsrc: require('./assets/blue-shoe.png'),
+          time:'5:00'
+        },
+        {
+          title: 'Song 3',
+          color: 'pink',
+          bgtext: 'MAX',
+          artist:'Someone good singer',
+          src:'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-8.mp3',
+          imgsrc: require('./assets/blue-shoe.png'),
+          time:'5:00'
+        },
+        {
+          title: 'Song 4',
+          color: 'pink',
+          bgtext: 'MAX',
+          artist:'Someone good singer',
+          src:'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-8.mp3',
+          imgsrc: require('./assets/pink-shoe.png'),
+          time:'5:00'
+        },
+        {
+          title: 'Song 5',
+          color: 'pink',
+          bgtext: 'MAX',
+          artist:'Someone good singer',
+          src:'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-8.mp3',
+          imgsrc: require('./assets/pink-shoe.png'),
+          time:'5:00'
+        },
+        {
+          title: 'Song 6',
+          color: 'pink',
+          bgtext: 'MAX',
+          artist:'Someone good singer',
+          src:'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-8.mp3',
+          imgsrc: require('./assets/pink-shoe.png'),
+          time:'5:00'
+        },
+        {
+          title: 'Song 7',
+          color: 'pink',
+          bgtext: 'MAX',
+          artist:'Someone good singer',
+          src:'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-8.mp3',
+          imgsrc: require('./assets/pink-shoe.png'),
+          time:'5:00'
+        },{
+          title: 'Song 8',
+          color: 'pink',
+          bgtext: 'MAX',
+          artist:'Someone good singer',
+          src:'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-8.mp3',
+          imgsrc: require('./assets/pink-shoe.png'),
+          time:'5:00'
+        },{
+          title: 'Song 9',
+          color: 'pink',
+          bgtext: 'MAX',
+          artist:'Someone good singer',
+          src:'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-8.mp3',
+          imgsrc: require('./assets/pink-shoe.png'),
+          time:'5:00'
+        },{
+          title: 'Song 10',
+          color: 'pink',
+          bgtext: 'MAX',
+          src:'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-8.mp3',
+          artist:'Someone good singer',
+          imgsrc: require('./assets/pink-shoe.png'),
+          time:'5:00'
+        },{
+          title: 'Song 11',
+          color: 'pink',
+          bgtext: 'MAX',
+          artist:'Someone good singer',
+          src:'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-8.mp3',
+          imgsrc: require('./assets/pink-shoe.png'),
+          time:'5:00'
+        },{
+          title: 'Song 12',
+          color: 'pink',
+          bgtext: 'MAX',
+          artist:'Someone good singer',
+          src:'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-8.mp3',
+          imgsrc: require('./assets/pink-shoe.png'),
+          time:'5:00'
+        },{
+          title: 'Song 13',
+          color: 'pink',
+          bgtext: 'MAX',
+          src:'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-8.mp3',
+          artist:'Someone good singer',
+          imgsrc: require('./assets/pink-shoe.png'),
+          time:'5:00'
+        },{
+          title: 'Song 14',
+          color: 'pink',
+          bgtext: 'MAX',
+          artist:'Someone good singer',
+          src:'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-8.mp3',
+          imgsrc: require('./assets/pink-shoe.png'),
+          time:'5:00'
+        },{
+          title: 'Song 15',
+          color: 'pink',
+          bgtext: 'MAX',
+          artist:'Someone good singer',
+          src:'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-8.mp3',
+          imgsrc: require('./assets/pink-shoe.png'),
+          time:'5:00'
+        },{
+          title: 'Song 16',
+          color: 'pink',
+          bgtext: 'MAX',
+          artist:'Someone good singer',
+          src:'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-8.mp3',
+          imgsrc: require('./assets/pink-shoe.png')
+        },{
+          title: 'Song 17',
+          color: 'pink',
+          bgtext: 'MAX',
+          artist:'Someone good singer',
+          src:'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-8.mp3',
+          imgsrc: require('./assets/pink-shoe.png')
+        },
+      ],
+      player:new Audio()
+    }
+  },
+  methods: {
+        play (product) {
+      if (typeof product.src != "undefined") {
+        this.current = product;
+        this.player.src = this.current.src;
+      }
+      this.player.play();
+      this.player.addEventListener('ended', function () {
+        this.index++;
+        if (this.index > this.products.length - 1) {
+          this.index = 0;
+        }
+        this.current = this.products[this.index];
+        this.play(this.current);
+      }.bind(this));
+      this.isPlaying = true;
+    },
+    pause () {
+      this.player.pause();
+      this.isPlaying = false;
+    },
+    next () {
+      this.index++;
+      if (this.index > this.products.length - 1) {
+        this.index = 0;
+      }
+      this.current = this.products[this.index];
+      this.play(this.current);
+    },
+    prev () {
+      this.index--;
+      if (this.index < 0) {
+        this.index = this.products.length - 1;
+      }
+      this.current = this.products[this.index];
+      this.play(this.current);
+    }
+  },
+  created () {
+    this.current = this.products[this.index];
+    this.player.src = this.current.src;
   }
 }
+
+
+  
 </script>
 
+    
+    
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  body {
+    font-family: 'Raleway', sans-serif;
+  }
+  main {
+    width: 100vw;
+    min-height: 100vh;
+    
+    background-color:#f8f8f8;
+    /*display: flex;*/
+    /* justify-content: center;
+    align-items: center; */
+  }
+  .products {
+    display: grid;
+    grid-template-columns: repeat(5,1fr);
+    max-width: 1280px;
+    padding: 25px;
+    margin: 0 auto;
+    border-radius: 25px;
+  }
+  .prev,.play,.pause,.next{
+    background-image: linear-gradient(to bottom right, #F444A4, #1168D4);
+    margin:5px;
+    padding:10px;
+    border: none;
+    border-radius: 10px;
+    cursor: pointer;
+  }
+  .fa{
+    color:white;  
+  }
+  .player{
+    margin: 40px 100px;
+    background: #fafafa;
+    box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+    padding: 25px;
+    border-radius: 25px;
+    align-content: center;
+    align-items: center;
+    justify-items: center;
+  }
+
+  #logo{
+    width: 15%;
+    padding: 15px;
+    margin: 10px;
+    
+  }
+
+  .head{
+   display: flex;
+   flex-direction: row;
+  }
+  #title{
+    vertical-align: -webkit-baseline-middle;
+    text-transform: uppercase;
+    font-weight: 750;
+    font-size: 80px;
+    align-items: center;
+    padding-top: 3%;
+  }
+  hr{
+    opacity:0.5;
+    width: 80%;
+    margin-left: 10%;
+  }
 </style>
---
+++ src/components/Product.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="product">
+    <div :class="'product-inner ' + product.color">
+      <div class="product-text-wrap">
+      </div>
+      <div class="product-image-wrap">
+        <img :src="product.imgsrc" class="image" />
+      </div>
+      <div class="product-detail">
+        <h2>{{ product.title }}</h2>
+        <p>{{product.artist}}</p>
+        <p>{{product.time}}</p>
+        
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'product',
+  props: ['product']
+}
+</script>
+
+<style>
+    .product {
+    /*flex: 1 1 33.333%;*/
+    width: 100%;
+    padding: 25px;
+    border-radius: 25px;
+  }
+  .product-inner {
+    position: relative;
+    padding: 25px;
+    box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+    perspective: 1000px;
+  }
+  .product-inner.green {
+    background-image: linear-gradient(to bottom right, #24D484, #116432);
+  }
+  .product-inner.blue {
+    background-image: linear-gradient(to bottom left, #24D484, #2474C4 70%);
+  }
+  .product-inner.pink {
+    background-image: linear-gradient(to bottom right, #F444A4, #1168D4);
+  }
+  .product-text-wrap {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: 0;
+    overflow: hidden;
+    perspective: 1000px;
+  }
+  
+  .product-text-wrap h2 {
+    color: #131313;
+    font-size: 128px;
+    font-weight: 900;
+    opacity: 0.2;
+    transform-origin: center;
+  }
+  .product-image-wrap {
+    position: relative;
+    z-index: 1;
+    transform-origin: center;
+  }
+  .product-image-wrap .image {
+    width: 100%;
+    filter: drop-shadow(0px 0px 12px rgba(0, 0, 0, 0.25));
+  }
+  .product-detail {
+    background-color: #f3f3f3;
+    padding: 25px;
+    margin: 0px -25px -25px;
+  }
+  .product-detail h2 {
+    font-size: 24px;
+    font-weight: 700;
+    color: #292929;
+    margin-bottom: 5px;
+  }
+  .product-detail p {
+    font-size: 14px;
+    font-weight: 300;
+    margin-bottom: 5px;
+    color: #282828;
+  }
+</style>
